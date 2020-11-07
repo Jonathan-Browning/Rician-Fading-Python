caa6261b55a2f7b511391e7b142747b6b1dad427
Jonathan Browning <jbrowning@live.com>
2020-11-07 09:39:14 +0000

    Changed figure displays
    Now displays images of the figures

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from Class.rice import Rice
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib
-matplotlib.use('TkAgg')
 from tkinter import messagebox
+import os
 
-def draw_envelope(canvas, data):
+def draw_envelope(data):
+    plotname = "envelope_plot.png"
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
     plt.figure(1)
@@ -28,22 +27,20 @@ def draw_envelope(canvas, data):
     plt.plot(data.xdataEnv[1:len(data.xdataEnv):2], data.ydataEnv[1:len(data.ydataEnv):2], "k.", label='Simulation') 
     leg = plt.legend(fontsize=15)
     leg.get_frame().set_edgecolor('k')
-    fig = plt.gcf()
-    figure_canvas_agg = FigureCanvasTkAgg(fig, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    plt.savefig(plotname)
     plt.close(1)
-    return figure_canvas_agg
+    return plotname
 
-def draw_phase(canvas, s):
+def draw_phase(s):
+    plotname = "phase_plot.png"
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
     plt.figure(1)
     plt.xlabel(r'$\theta$', fontsize=18)
     plt.ylabel(r'$f_{\Theta}(\theta)$', fontsize=18)
     plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
-               [r'$-\pi$',r'$-\pi/2$',r'$0$',r'$\pi/2$',r'$\pi$'],
-               fontsize = 18)
+                [r'$-\pi$',r'$-\pi/2$',r'$0$',r'$\pi/2$',r'$\pi$'],
+                fontsize = 18)
     plt.yticks(fontsize = 18) 
     plt.xlim((-np.pi, np.pi))
     plt.ylim(bottom=0)
@@ -53,16 +50,9 @@ def draw_phase(canvas, s):
     plt.plot(s.xdataPh[1:len(s.xdataPh):2], s.ydataPh[1:len(s.ydataPh):2], "k.", label='Simulation')  
     leg = plt.legend(fontsize=15)
     leg.get_frame().set_edgecolor('k')
-    fig = plt.gcf()
-    figure_canvas_agg = FigureCanvasTkAgg(fig, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    plt.savefig(plotname)
     plt.close(1)
-    return figure_canvas_agg
-
-def delete_figure_agg(figure_agg):
-    figure_agg.get_tk_widget().forget()
-    plt.close('all')
+    return plotname
     
 def main():   
         
@@ -73,8 +63,8 @@ def main():
           [sg.Text("\phi:", size=(8, 1), font='Helvetica 18'), sg.Input(key=r'-\phi', size=(5, 1), font='Helvetica 18')],      
           [sg.Button('Calculate', font='Helvetica 18'), sg.Exit(font='Helvetica 18')],
           [sg.Text("Time (s):", size=(8, 1), font='Helvetica 18'), sg.Txt('', size=(8,1), key='output')],
-          [sg.Canvas(key='-CANVAS1')],
-          [sg.Canvas(key='-CANVAS2')],]
+          [sg.Image(key='-Image1')],
+          [sg.Image(key='-Image2')]]
         
     window = sg.Window("The Rician fading model", layout, finalize=True, font='Helvetica 18')
 
@@ -86,13 +76,6 @@ def main():
         # Close if the exist button is pressed or the X
         if event in (sg.WIN_CLOSED, 'Exit'):
             break      
-        
-        # check for previous figures and delete them
-        if 'fig_canvas_agg1' in locals():
-           delete_figure_agg(fig_canvas_agg1)
-        
-        if 'fig_canvas_agg2' in locals():
-           delete_figure_agg(fig_canvas_agg2)
         
         # Rice class instance which calculates everything and time the exeuction
         start = time.time()
@@ -108,9 +91,14 @@ def main():
         window['output'].update(exeTime)  # Display the execution time 
         
         # drawing the figures
-        fig_canvas_agg1 = draw_envelope(window['-CANVAS1'].TKCanvas, s)
-        fig_canvas_agg2 = draw_phase(window['-CANVAS2'].TKCanvas, s)
+        imageFileName1 = draw_envelope(s)
+        window.Element('-Image1').Update(imageFileName1)
+        os.remove(imageFileName1) # need to remove the image file again
         
+        imageFileName2 = draw_phase(s)
+        window.Element('-Image2').Update(imageFileName2)
+        os.remove(imageFileName2) # need to remove the image file again
+
     window.close()
     
 if __name__ == "__main__":
